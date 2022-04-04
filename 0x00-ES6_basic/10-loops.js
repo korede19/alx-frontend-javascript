8fbdab57ebb3f8fddb0513416a6e66ad22d8ae3d
unknown <oyeyemikorede5@gmail.com>
2022-04-04 17:48:26 +0100

    alx-frontend-javascript

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    const returnArray = [];
+    for (const element of array) {
+      returnArray.push(appendString + element);
+    }
+  
+    return returnArray;
+}
