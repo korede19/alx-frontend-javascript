8a453023e4056f211803daf06994dd213167659a
unknown <oyeyemikorede5@gmail.com>
2022-04-06 20:29:55 +0100

    alx-frontend-javascript

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    signUpUser(firstName, lastName);
+    uploadPhoto(fileName); 
+}
---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default function divideFunction(numerator, denominator) {
+    if (denominator !== 0) {
+        return (numerator / denominator);
+    } else {
+        throw new Error('cannot divide by 0');
+    }
+}
